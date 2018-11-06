1710affbdd2fc5482aedc2b90855bf53c1167e01
issarapong <cyber_freedom@hotmail.com>
2018-11-06 12:02:38 +0700

    Update Dir-size-command.sh

--- Dir-size-command.sh
+++ Dir-size-command.sh
@@ -3,3 +3,9 @@ du -sh #/Current size
 du -sh --  # /list all folder size
 
 du -H --max-depth=1 /home/      #Sort Folders by Size 
+ex
+920K	/etc/apache2
+612K	/etc/ssh
+404K	/etc/sysconfig
+224K	/etc/ImageMagick
+200K	/etc/munin
