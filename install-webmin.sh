fa12dafeaf40fd3e21625a73c75e4bde42bca02c
issarapong <cyber_freedom@hotmail.com>
2018-11-07 10:52:28 +0700

    Create install-webmin.sh

---
+++ install-webmin.sh
@@ -0,0 +1,8 @@
+sudo -s
+echo "deb https://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
+cd /root
+wget http://www.webmin.com/jcameron-key.asc
+apt-key add jcameron-key.asc
+apt-get install apt-transport-https
+apt-get update
+apt-get install webmin
